b1cca87cca1d4815cbc75f5a7ca5d31db98d2bbb
manglobe <manglobegit@gmail.com>
2018-06-12 13:04:28 +0800

    删除nwb-sass 新增sass-loader,node-sass依赖，支持node v10.x

--- nwb.config.js
+++ nwb.config.js
@@ -1,26 +1,55 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  type: 'react-component',
+  type: "react-component",
   npm: {
     esModules: true,
     umd: false
   },
   webpack: {
-    aliases:{
-      '@src': path.resolve('src'),
-      '@lib': path.resolve('lib'),
-      '@es': path.resolve('es'),
-      '@media': path.resolve('src/media'),
+    aliases: {
+      "@src": path.resolve("src"),
+      "@lib": path.resolve("lib"),
+      "@es": path.resolve("es"),
+      "@media": path.resolve("src/media")
+    },
+    // cssPreprocessors: {
+    //   sass: {
+    //     test: /\.s[ac]ss$/,
+    //     loader: require.resolve('sass-loader'),
+    //     defaultConfig: 'sassLoader'
+    //   }
+    // },
+    extra: {
+      module: {
+        rules: [
+          {
+            test: /\.scss$/,
+            use: [
+              "style-loader",
+              {
+                loader: "css-loader",
+                options: {
+                  // 使用CSS Modules
+                  modules: true,
+                  localIdentName: "[local]-[hash:base64:6]-[hash:base32:4]",
+                  sourceMap: true
+                }
+              },
+              "sass-loader"
+            ]
+          }
+        ]
+      }
     },
     rules: {
-      'sass-css': {
-        modules: true,
-        localIdentName: '[name]__[local]__[hash:base64:5]'
-      },
+      // 'sass-css': {
+      //   modules: true,
+      //   localIdentName: '[name]__[local]__[hash:base64:5]'
+      // },
       svg: {
-        use: ['svgr/webpack', 'url-loader']
+        use: ["svgr/webpack", "url-loader"]
       }
     }
   }
-}
+};
