250aba9d47eb7204c3e38be64f97da2879227c3e
manglobe <manglobegit@gmail.com>
2018-06-12 15:23:23 +0800

    恢复 Detail

--- demo/src/Detail/index.js
+++ demo/src/Detail/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 // import Components from "@lib";
-import Components from "@lib";
-import pop from "@src/tools/pop";
-import { Warn } from '@lib/components/Message' 
+import Components from "@src";
+// import pop from "@src/tools/pop";
+// import { Warn } from '@lib/components/Message' 
 // import pop from "wowjoy-component/lib/tools/pop";
 const PROPS = {
 
@@ -10,13 +10,13 @@ const PROPS = {
 
 class Detail extends Component {
   componentDidMount() {
-    const ComponentItem = Components[this.name];
-    const popNode = pop(Warn, {content:'asd', onClose:()=>{alert('close')}})({
-      animeShow:'slideRightIn',
-      autoClose: 2000,
-      animeHide:'slideRightOut',
-      layer:true
-    })    
+    // const ComponentItem = Components[this.name];
+    // const popNode = pop(Warn, {content:'asd', onClose:()=>{alert('close')}})({
+    //   animeShow:'slideRightIn',
+    //   autoClose: 2000,
+    //   animeHide:'slideRightOut',
+    //   layer:true
+    // })    
   }
   
   
