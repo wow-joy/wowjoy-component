35e3a7a9f6b9f12636339311b6962bfe95840a74
manglobe <manglobegit@gmail.com>
2018-06-20 15:10:35 +0800

    v1.5.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,47 +1,65 @@
+## 1.5.0 (2018/6/20)
+
+### feature
+
+- 新增 form 方法
+
+### docs
+
+- 新增 form 的说明文档
 
 ## 1.4.0 (2018/6/13)
+
 ### feature
-- 新增dialog组件 
-### bug fix 
+
+- 新增 dialog 组件
+
+### bug fix
+
 - 修复了初始化脚本的一个引用异常
+
 ### refactor
-- pop方法不再接受两个参数，而是直接接受一个实例化的组件作为参数
-- Message组件从props.content接收内容，修改为props.children接收
-- 自动初始化脚本，现在会额外初始化一份readme.md
+
+- pop 方法不再接受两个参数，而是直接接受一个实例化的组件作为参数
+- Message 组件从 props.content 接收内容，修改为 props.children 接收
+- 自动初始化脚本，现在会额外初始化一份 readme.md
+
 ### docs
+
 - 修改了上述改动的相应文档
 - 修复几个文档的文案异常
 
 ## 1.3.3 (2018/6/11)
-- 删除了nwb-sass包
-- 修复node v10.x支持
+
+- 删除了 nwb-sass 包
+- 修复 node v10.x 支持
 
 ## 1.3.2 (2018/6/11)
+
 - 修复文档错误
 
 ## 1.3.1 (2018/6/11)
+
 - 修复文档错误
-- 增加peerDependencies
+- 增加 peerDependencies
 
 ## 1.3.0 (2018/6/11)
 
-- 新增pop方法
-- 新增pubSub方法
-- 新增Message组件
+- 新增 pop 方法
+- 新增 pubSub 方法
+- 新增 Message 组件
 - 新增自动初始化组件脚本
-- 提取公用Hoc方法
+- 提取公用 Hoc 方法
 
-TODO: dialog组件
-
+TODO: dialog 组件
 
 ## 1.1.0 (2018/6/8)
 
-- 新增btn组件
-- 优化layout组件
-    - 增加布尔类型支持，允许设置header,aside,main,footer为false以控制显隐。
-
+- 新增 btn 组件
+- 优化 layout 组件
+  - 增加布尔类型支持，允许设置 header,aside,main,footer 为 false 以控制显隐。
 
 ## 1.0.0 (2018/6/7)
 
 - 项目初始化
-- 新增Layout组件
+- 新增 Layout 组件
--- demo/src/Detail/index.js
+++ demo/src/Detail/index.js
@@ -1,48 +1,16 @@
 import React, { Component } from "react";
-// import Components from "@lib";
 import Components from "@src";
-// import pop from "@src/tools/pop";
-// import { Warn } from '@lib/components/Message'
-// import pop from "wowjoy-component/lib/tools/pop";
-import createForm from "@src/tools/forms";
 const PROPS = {};
 
 class Detail extends Component {
-  state = {
-    value: 0
-  };
-  componentDidMount() {
-    // const ComponentItem = Components[this.name];
-    // const popNode = pop(Warn, {content:'asd', onClose:()=>{alert('close')}})({
-    //   animeShow:'slideRightIn',
-    //   autoClose: 2000,
-    //   animeHide:'slideRightOut',
-    //   layer:true
-    // })
-  }
-
   render() {
-    const { match, formHandle, bindForm } = this.props;
-    window.formHandle = formHandle;
+    const { match } = this.props;
     const name = match.params.name.replace(
       /^(.)(.*)$/,
       (match, $1, $2) => $1.toUpperCase() + $2
     );
     this.name = name;
     const ComponentItem = Components[name];
-    return (
-      <input
-        {...bindForm({
-          name: "xx",
-          onChange: e => this.setState({ value: e.target.value }),
-          value: this.state.value,
-          // defaultValue:  this.state.value,
-          rules: v => v === "1"
-        })}
-        // onBlur={e => console.log(this.setState({ value: 2 }))}
-        type="text"
-      />
-    );
 
     if (ComponentItem) {
       return <ComponentItem {...PROPS}>ComponentItem</ComponentItem>;
