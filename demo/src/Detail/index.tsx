fc8507d5514fbefc1054aa27075b613f95a8565d
manglobe <manglobegit@gmail.com>
2019-04-28 15:21:36 +0800

    updemo

--- demo/src/Detail/index.tsx
+++ demo/src/Detail/index.tsx
@@ -13,10 +13,52 @@ export interface Props {
 }
 export interface State {
   visible: boolean;
+  value?: any;
 }
-
+class Dad extends React.Component<
+  { c?: any; children: any; updater?: any },
+  {}
+> {
+  constructor(props: any, context: any, updater: any) {
+    super(props);
+    console.log(updater);
+  }
+  // shouldComponentUpdate(nextProps: any, nextState: any) {
+  //   console.log(1)
+  //   console.log(nextProps.children.props);
+  //   console.log(this.props.children.props);
+  //   console.log(this.props === nextProps);
+  //   console.log(this.props.children === nextProps.children);
+  //   return true;
+  // }
+  render() {
+    console.log("dad render");
+    return <div>{this.props.children}</div>;
+  }
+}
+class Son extends React.PureComponent<{ value?: any }, {}> {
+  render() {
+    console.log("son render");
+    return <input value={this.props.value} readOnly />;
+  }
+}
+const App = () => {
+  const [value, setState] = React.useState(0);
+  return (
+    <>
+      <Dad
+        updater={(...args: any[]) => {
+          console.log(args);
+        }}
+      >
+        <Son />
+      </Dad>
+      <span onClick={() => setState(value + 1)}>{value}</span>
+    </>
+  );
+};
 class Detail extends React.Component<Props, State> {
-  state = { visible: false };
+  state = { visible: false, value: 0 };
   componentDidMount() {
     // window.onclick = () => {
     //   this.setState({ visible: true });
@@ -28,6 +70,11 @@ class Detail extends React.Component<Props, State> {
   close = () => {
     this.setState({ visible: false });
   };
+  clickHandle = () => {
+    this.setState({
+      value: this.state.value + 1
+    });
+  };
   render() {
     const { match } = this.props;
     const name = match.params.name.replace(
@@ -37,6 +84,15 @@ class Detail extends React.Component<Props, State> {
     // this.name = name;
     const ComponentItem = Components[name];
 
+    return <App />;
+    return (
+      <>
+        <Dad>
+          <Son value={this.state.value} />
+        </Dad>
+        <span onClick={this.clickHandle}>{this.state.value}</span>
+      </>
+    );
     if (ComponentItem) {
       return (
         <ComponentItem {...PROPS} {...this.state} onClose={this.close}>
